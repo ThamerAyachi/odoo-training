0846f9f37f4cbb46463d1e497408fd047fb87ef0
ThamerAyachi <thamerjak3@gmail.com>
2023-11-15 17:42:39 +0100

    15/11/2023

--- custom_addons/estate/models/estate_property_offer.py
+++ custom_addons/estate/models/estate_property_offer.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields
+from odoo import models, fields, api
+from datetime import date, datetime, timedelta
 
 
 class EstatePropertyOffer(models.Model):
@@ -20,3 +21,26 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id = fields.Many2one("res.partner", required=True)
     property_id = fields.Many2one("estate.property", required=True)
+    validity = fields.Integer('validity(days)', default=7)
+    create_date = fields.Date('Create Date')
+    date_deadline = fields.Date(
+        'Deadline', compute="_compute_deadline", inverse="_inverse_deadline")
+
+    @api.depends('create_date', 'validity')
+    def _compute_deadline(self):
+        for record in self:
+            if not record.create_date:
+                record.create_date = None
+            else:
+                record.date_deadline = record.create_date + \
+                    timedelta(days=record.validity)
+
+    def _inverse_deadline(self):
+        for record in self:
+            if not record.create_date:
+                record.create_date = None
+            if not record.date_deadline:
+                record._compute_deadline()
+            else:
+                validity_1 = record.date_deadline-record.create_date
+                record.validity = validity_1.days
