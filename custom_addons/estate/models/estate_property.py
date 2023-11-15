8fd6e4ad16d69dce4c3657b96e9a7e7db3b872fd
ThamerAyachi <thamerjak3@gmail.com>
2023-11-15 17:24:41 +0100

    15/11/2023

--- custom_addons/estate/models/estate_property.py
+++ custom_addons/estate/models/estate_property.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields
+from odoo import models, fields, api
 from dateutil.relativedelta import relativedelta
 
 
@@ -19,11 +19,11 @@ class EstateProperty(models.Model):
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer(string="Living Area (sqm)")
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
+    garden_area = fields.Integer(string="Garden Area (sqm)")
     garden_orientation = fields.Selection(
         selection=[
             ('north', 'North'),
@@ -52,3 +52,23 @@ class EstateProperty(models.Model):
         'estate.property.tag', string="Tags")
     offer_ids = fields.One2many(
         "estate.property.offer", "property_id", string="Offer")
+    total_area = fields.Float(
+        compute="_compute_total_area", readonly=True, string="Total Area (sqm)")
+    best_price = fields.Float(
+        compute="_compute_best_price", readonly=True, string="Best Offer")
+
+    @api.depends("living_area", "garden_area")
+    def _compute_total_area(self):
+        """Function to calculate Total Area"""
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    @api.depends("offer_ids")
+    def _compute_best_price(self):
+        """Function to Find Best Price"""
+        for record in self:
+            offer_price = record.offer_ids.mapped('price')
+            if not offer_price:
+                record.best_price = None
+            else:
+                record.best_price = max(offer_price)
