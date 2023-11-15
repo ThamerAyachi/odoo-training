52f8771a7fb3c835e10dd54ea2f57b4691338fdf
ThamerAyachi <thamerjak3@gmail.com>
2023-11-15 15:08:24 +0100

    15/11/2023

--- custom_addons/estate/models/estate_property.py
+++ custom_addons/estate/models/estate_property.py
@@ -45,3 +45,6 @@ class EstateProperty(models.Model):
         default='new'
     )
     property_type_id = fields.Many2one("estate.property.type", string="Type")
+    seller_id = fields.Many2one(
+        "res.users", string="Salesman", default=lambda self: self.env.user)
+    buyer_id = fields.Many2one("res.partner", copy=False, string="Bayer")
